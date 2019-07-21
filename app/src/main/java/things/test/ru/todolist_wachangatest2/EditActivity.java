a1e0c5daa27d3a2f28cb43e8370f3e9219cfa99c
Vlad <lemon444eg@gmail.com>
2019-07-21 20:12:20 +0700

    get started with notifications

--- app/src/main/java/things/test/ru/todolist_wachangatest2/EditActivity.java
+++ app/src/main/java/things/test/ru/todolist_wachangatest2/EditActivity.java
@@ -1,7 +1,13 @@
 package things.test.ru.todolist_wachangatest2;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.BitmapFactory;
 import android.support.design.widget.TextInputEditText;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatImageButton;
@@ -9,6 +15,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
@@ -22,6 +30,7 @@ public class EditActivity extends AppCompatActivity implements DatabaseCallback{
     AppDatabase db;
     TextInputEditText text_field;
     AppCompatImageButton delete_button;
+    private static final int NOTIFY_ID = 228;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +68,9 @@ public class EditActivity extends AppCompatActivity implements DatabaseCallback{
 
     }
 
+
+    //button.setEnabled(false);
+
     public void save_exit(View view) {          //if length=0, ne save
 
         String new_task_text=text_field.getText().toString();
@@ -80,6 +92,7 @@ public class EditActivity extends AppCompatActivity implements DatabaseCallback{
             else{
                 exit();
             }
+
 
         }
 
@@ -126,4 +139,68 @@ public class EditActivity extends AppCompatActivity implements DatabaseCallback{
         startActivity(intent);
     }
 
+    public void setNotification(View view) {
+
+        String new_task_text=text_field.getText().toString();
+        String notificationText = new String();
+
+        boolean isSet;
+
+        System.out.println(new_task_text);
+
+        if(this_task==null){
+            if(new_task_text.length()!=0){
+                notificationText=new_task_text.substring(0,Math.min(new_task_text.length(),19));
+
+                dbmanager.addTask(this,new_task_text);
+
+                //System.out.println(notificationText);
+            }
+            else{
+                return;
+            }
+        }
+        else{
+            if(new_task_text.length()!=0){
+                notificationText=new_task_text.substring(0,Math.min(new_task_text.length(),19));
+                this_task.text=new_task_text;
+                dbmanager.updateTask(this,this_task);
+
+            }
+            else{
+                return;
+            }
+        }
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this,
+                0, notificationIntent,
+                PendingIntent.FLAG_CANCEL_CURRENT);
+
+        Resources res = this.getResources();
+
+        // до версии Android 8.0 API 26
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+
+        builder.setContentIntent(contentIntent)
+                .setSmallIcon(R.drawable.ic_stat_name)
+                //.setContentTitle(res.getString(R.string.notifytitle)) // Заголовок уведомления
+                .setContentTitle("Напоминание")
+                //.setContentText(res.getString(R.string.notifytext))
+                .setContentText(notificationText) // Текст уведомления
+                // необязательные настройки
+                .setLargeIcon(BitmapFactory.decodeResource(res, R.drawable.ic_stat_name)) // большая
+                // картинка
+                .setWhen(System.currentTimeMillis())
+                .setAutoCancel(true); // автоматически закрыть уведомление после нажатия
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        // Альтернативный вариант
+        // NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        //notificationManager.cancel(NOTIFY_ID);
+
+        notificationManager.notify(NOTIFY_ID, builder.build());
+    }
 }
